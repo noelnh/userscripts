a4bed2edc28d029ca17a9e5fa41066cbd4188190
Noel NHato <noelnhato@gmail.com>
2017-04-18 01:32:38 +0900

    stacc v0.2

--- Pixiv_Touch_Moe_Stacc.user.js
+++ Pixiv_Touch_Moe_Stacc.user.js
@@ -3,15 +3,15 @@
 // @namespace   noelpinch
 // @description For Pixiv mobile
 // @include     https://touch.pixiv.net/stacc*
-// @version     0.1
+// @version     0.2
 // @grant       none
 // ==/UserScript==
 //
 
 (function() {
-    console.log('pixiv touch moe stacc v1');
+    console.log('pixiv touch moe stacc v0.2');
 
-    // Global style: img.height -> auto!
+    // Global style: imgbox size -> unset
     var css = '.stacclist li.illust div.imgbox.bookmark img {max-height: unset; max-width: unset;}',
         head = document.head || document.getElementsByTagName('head')[0],
         style = document.createElement('style');
@@ -23,29 +23,36 @@
     }
     head.appendChild(style);
 
+    // bookmark
     var imglist = document.getElementsByClassName('imgbox bookmark');
     var count = 0;
+
+    // follow
+    var imglist2 = document.getElementsByClassName('work withimage');
+    var count2 = 0;
 
     var replace_imgs = function() {
         for (; count < imglist.length; count++) {
             var imgs = imglist[count].getElementsByTagName("img");
             if (imgs.length <= 0) continue;
-            if (imgs[0].src.indexOf('data:') == 0) {
-                break;
-            }
             if (imgs[0].src.indexOf('240x480') < 0) {
                 continue;
             }
             imgs[0].src = imgs[0].src.replace('240x480', '480x960');
+        }
+        for (; count2 < imglist2.length; count2++) {
+            var imgs = imglist2[count2].getElementsByClassName("image");
+            imgs[0].style.backgroundImage = imgs[0].style.backgroundImage.replace('260x260', '600x600');
         }
     }
     replace_imgs();
 
     var observer;
     var articles = document.getElementById('articles');
-    if (imglist.length > 0) {
+    if (imglist.length > 0 || imglist2.length > 0) {
         observer = new MutationObserver(function(mutations) {
             imglist = document.getElementsByClassName('imgbox bookmark');
+            imglist2 = document.getElementsByClassName('work withimage');
             replace_imgs();
         });
         observer.observe(articles, { childList: true, subtree: true, attributes: true });
