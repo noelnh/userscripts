5921239b2ac9325b8a690c043c14fff898eb94a4
Noel NHato <noelnhato@gmail.com>
2017-04-18 19:42:49 +0900

    mstdn v0.2

--- Mastodon_Tweak.user.js
+++ Mastodon_Tweak.user.js
@@ -7,13 +7,13 @@
 // @include     https://mstdn.jp/*
 // @include     https://mstdn.io/*
 // @include     https://mstdn.*.org/*
-// @version     0.1
+// @version     0.2
 // @grant       none
 // ==/UserScript==
 
 
 (function() {
-    console.log('mastondon tweak v1');
+    console.log('mastondon tweak v0.2');
 
     var body = document.getElementsByTagName('body')[0];
     var column_width = (body.offsetWidth - 300) / 3;
@@ -29,5 +29,36 @@
         style.appendChild(document.createTextNode(css));
     }
     head.appendChild(style);
+
+
+    // Swap columns
+    var swap_columns = function() {
+        var columns_area = document.getElementsByClassName('columns-area');
+        var columns = document.getElementsByClassName('column');
+        if (columns_area.length > 0 && columns.length > 1) {
+            var bells = columns[1].getElementsByClassName('fa-bell');
+            if (bells.length > 0) {
+                columns_area[0].append(columns[1]);
+            }
+        }
+    }
+    var observer = new MutationObserver(function(mutations) {
+        swap_columns();
+    });
+
+    document.onreadystatechange = () => {
+        if (document.readyState === 'complete') {
+
+            swap_columns();
+
+            // Create observer
+            var columns_area = document.getElementsByClassName('columns-area');
+            if (columns_area.length > 0) {
+                observer.observe(columns_area[0],
+                    { childList: true, subtree: true, attributes: true });
+                console.log('Column observer created');
+            }
+        }
+    };
 
 })();
